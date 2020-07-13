8958bfd1f86baa562373f95a4e88beaeee39d5b5
Lyndi Castrejon <lyndic@protonmail.com>
2020-07-12 20:01:57 -0500

    Initial commit; first design draft

---
+++ src/components/BottomScreen.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+// import './BottomScreenStyles.css';
+
+class BottomScreen extends Component {
+    render() {
+        return(
+            <>
+                <div className="bottom-container">
+                    <div className="bottom-screen"></div>
+                </div>
+            </>
+        )
+    }
+}
+
+export default BottomScreen;
