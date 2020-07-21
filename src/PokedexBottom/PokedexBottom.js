03bf58a384014a1dbf38204735cbfffc85167fed
Lyndi Castrejon <lyndic@protonmail.com>
2020-07-20 19:40:22 -0500

    Add whitespace between stats and data, remove unnecessary logic, and add comments

--- src/PokedexBottom/PokedexBottom.js
+++ src/PokedexBottom/PokedexBottom.js
@@ -2,25 +2,26 @@ import React from 'react'
 
 import PreviousArrow from './PreviousArrow';
 import NextArrow from './NextArrow';
+
 import './PokedexBottom.css';
 
-export default function PokedexBottom({pokemon, goToPrev, goToNext, setCurrentPokemon}) {
+/*
+**  Structure of bottom half of Pokédex
+*/
+export default function PokedexBottom({pokemon, goToPrev, goToNext}) {
     return (
         <div className="bottom-container">
-            {/* {switchToPrevPokemon && <div className="arrow left"></div>} */}
             <PreviousArrow goToPrev={goToPrev} />
             <div className="bottom-screen">
                 {pokemon.map(p => (
                     <div key={p} className="pokemon-stats">
-                        <p>Name: {p.res.name}</p>
-                        <p>Height: {p.res.height}</p>
-                        <p>Weight: {p.res.weight}</p>
+                        <p>Name: &nbsp;{p.res.name}</p>
+                        <p>Height: &nbsp;{p.res.height}</p>
+                        <p>Weight: &nbsp;{p.res.weight}</p>
                     </div>
-                    
                 ))}
             </div>
             <NextArrow goToNext={goToNext} />
-            {/* {goToNext && <div className="arrow right"></div>} */}
         </div>
     )
 }
